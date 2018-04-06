7718062e76510221e001ce4804570cc7176ff1df
PawelJa <janecki.paw@gmail.com>
2018-04-06 12:54:28 +0200

    WebInitializer

--- src/main/java/pl/springsecurityrest/config/WebInitializer.java
+++ src/main/java/pl/springsecurityrest/config/WebInitializer.java
@@ -1,4 +1,23 @@
 package pl.springsecurityrest.config;
 
-public class WebInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
 }
